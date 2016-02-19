affdf1f8d1639face5f744ab9c13e84de6bfcd2c
Vishal <vishalraj48@gmail.com>
2016-02-19 12:00:18 -0800

    First Commit

---
+++ app/src/main/java/in/ernet/iitg/app/Show_accelrometer.java
@@ -0,0 +1,41 @@
+package in.ernet.iitg.app;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.nfc.NfcAdapter;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Show_accelrometer extends Activity implements SensorEventListener {
+    Sensor accelerometer;
+    SensorManager sm;
+    TextView acceleration;
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_show_accelrometer);
+        sm=(SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer=sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //TextView T= (TextView)findViewById(R.id.parameters);
+        sm.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        acceleration= (TextView)findViewById(R.id.accelrometer);
+    }
+
+    public void onSensorChanged(SensorEvent event){
+        acceleration.setText("X: " + event.values[0] +"\nY: " +  event.values[1] + "\nZ: " + event.values[2] );
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
+    }
+}
---
+++ app/src/main/java/in/ernet/iitg/app/Show_magnetometer.java
@@ -0,0 +1,47 @@
+package in.ernet.iitg.app;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.nfc.NfcAdapter;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.Console;
+
+public class Show_magnetometer extends Activity implements SensorEventListener {
+
+    Sensor magnetometer;
+    SensorManager sm;
+    TextView mag;
+    private static final String TAG = "vishal";
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_show_accelrometer);
+        sm=(SensorManager)getSystemService(SENSOR_SERVICE);
+        magnetometer=sm.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        sm.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mag= (TextView)findViewById(R.id.Magnetometer);
+    }
+    //final private String tag =  vishal;
+    public void onSensorChanged(SensorEvent event){
+        Log.d("I am Here",TAG);
+        Log.d(String.valueOf(event.values), TAG);
+        //mag.setText("values  " + event.values.length);
+        //mag.setText("X: " + String.valueOf(event.values[0]) );
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
+    }
+
+}
