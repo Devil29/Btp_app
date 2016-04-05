711965444873a28dc660b361b46e3436f080c540
Vishal <vishalraj48@gmail.com>
2016-04-04 21:54:33 -0700

    ALL sensor to be updated Simuntaneously

--- app/src/main/java/in/ernet/iitg/app/Show_All.java
+++ app/src/main/java/in/ernet/iitg/app/Show_All.java
@@ -1,19 +1,25 @@
 package in.ernet.iitg.app;
 
-import android.content.Intent;
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class Testing_Data extends AppCompatActivity {
-
+public class Show_All extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_testing__data);
+        setContentView(R.layout.activity_show__all);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,24 +31,5 @@ public class Testing_Data extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    public void onAccelrometer(View view){
-        Intent i = new Intent(this, Show_accelrometer.class);
-        startActivity(i);
-    }
-
-    public void onMagnetometer(View view){
-        Intent i = new Intent(this, Show_magnetometer.class);
-        startActivity(i);
-    }
-
-    public void onRotation(View view){
-        Intent i = new Intent(this, Show_rotation.class);
-        startActivity(i);
-    }
-    public void onALL(View view){
-        Intent i=new Intent(this,Show_All.class);
-        startActivity(i);
     }
 }
