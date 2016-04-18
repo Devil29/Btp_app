7517cca0cc7de96c2142c8d18c878ca82ee10493
Vishal <vishalraj48@gmail.com>
2016-04-18 13:18:50 -0700

    Started Sending data to server part

--- app/src/main/java/in/ernet/iitg/app/update_Data.java
+++ app/src/main/java/in/ernet/iitg/app/update_Data.java
@@ -1,29 +1,136 @@
 package in.ernet.iitg.app;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.app.Activity;
+import android.widget.Button;
 
-public class update_Data extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class update_Data extends Activity {
+
+    private ProgressDialog pDialog;
+    JSONObject jsonObject = new JSONObject();
+    private static String url_Send_Data = "http://10.9.3.30/BTP/Send_Data.php";
+    private static final String TAG_SUCCESS = "success";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update__data);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // Create button
+        Button btnCreateProduct = (Button) findViewById(R.id.update_Start);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // button click event
+        btnCreateProduct.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // creating new product in background thread
+                new SendData().execute();
             }
         });
     }
+    class SendData extends AsyncTask<String, String, String> {
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(update_Data.this);
+            pDialog.setMessage("Sending Data..");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        protected String doInBackground(String... args) {
+            try{
+                jsonObject.put("DATA" ,"ALL_data");
+            }
+            catch (JSONException e){
+                e.printStackTrace();
+            }
+            HttpURLConnection urlConnection = null;
+            try{
+                URL url = new URL(url_Send_Data);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestProperty("Accept", "application/json");
+                Writer writer = new BufferedWriter(new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8"));
+                writer.write(String.valueOf(jsonObject));
+                writer.close();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+            /*
+            String name = inputName.getText().toString();
+            String price = inputPrice.getText().toString();
+            String description = inputDesc.getText().toString();
+
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("name", name));
+            params.add(new BasicNameValuePair("price", price));
+            params.add(new BasicNameValuePair("description", description));
+
+            // getting JSON Object
+            // Note that create product url accepts POST method
+            JSONObject json = jsonParser.makeHttpRequest(url_create_product,
+                    "POST", params);
+
+            // check log cat fro response
+            Log.d("Create Response", json.toString());
+
+            // check for success tag
+            try {
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // successfully created product
+                    Intent i = new Intent(getApplicationContext(), AllProductsActivity.class);
+                    startActivity(i);
+
+                    // closing this screen
+                    finish();
+                } else {
+                    // failed to create product
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+*/
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            pDialog.dismiss();
+        }
+
+    }
 }
